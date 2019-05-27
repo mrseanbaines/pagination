0f23d4c8a5419a4823874d7e335d08ab7b4af82a
Sean Baines <mrseanbaines@gmail.com>
2019-05-27 13:36:44 +0100

    Configure webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', path.resolve(__dirname, 'src')],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
